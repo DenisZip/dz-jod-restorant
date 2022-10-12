6e609712da4df11c4aa7041e5b4191d157acc3ef
DenisZip <112699948+DenisZip@users.noreply.github.com>
2022-10-12 17:46:20 +0600

    Add files via upload

---
+++ script/basket.js
@@ -0,0 +1,16 @@
+var plus = document.getElementsByClassName('js-plus-btn')[0];
+var minus = document.getElementsByClassName('js-minus-btn')[0];
+var field = document.getElementById('number-diet');
+
+plus.addEventListener('click', function(){ 
+  field.value = Calc(1); 
+});
+minus.addEventListener('click', function(){
+  field.value = Calc(-1);  
+});
+
+function Calc(oper) {
+  let result = parseInt(field.value) + oper ; 
+  return (result < 1) ? 1 : result;
+}
+
